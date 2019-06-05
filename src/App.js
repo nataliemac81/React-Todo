dfec5c7ff30f8adf964850b25a6846a4f9696625
Natalie McCroy <nataliemccroy@gmail.com>
2019-06-04 20:19:15 -0500

    added heading for Todo list app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+const TodoHeading = props => <h1>{props.message}!</h1>;
+
+
 class App extends React.Component {
+  constructor() { 
+    super();
+    this.state = {
+      message: "Todo List: MVP"
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoHeading message={this.state.message}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,4 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+// const TodoListMessage = props => <h1>Todo List, {props.message}!</h1>;
